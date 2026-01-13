dabd51f9fe5f5e1891a41b9aa8c206f61111517f
Abdulrahman Habeeb <habeebabdul383@gmail.com>
2026-01-13 17:20:07 +0100

    fix:add frontend domain name to list of allowed origins to fix CORS issue

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://elia-noninoculative-curt.ngrok-free.dev",
+  "https://hot-takes-restaurant-frontend.vercel.app",
   "https://1jwgr90t-5173.uks1.devtunnels.ms",
 ];
 export default allowedOrigins;
